de3b9d090b8ad7f378056c328c091dc8c7729686
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2015-04-24 00:26:31 +0200

    Formatted source code and replaced spaces by tabs.

--- Config.m
+++ Config.m
@@ -2,16 +2,28 @@
 
 
 (* these settings determine what to load when FeynCalc starts *)
-If[!ValueQ[Global`$LoadFeynArts], Global`$LoadFeynArts = False];
-If[!ValueQ[Global`$LoadPhi], Global`$LoadPhi = False];
-If[!ValueQ[Global`$LoadTARCER], Global`$LoadTARCER = False];
+If[ !ValueQ[Global`$LoadFeynArts],
+	Global`$LoadFeynArts = False
+];
+If[ !ValueQ[Global`$LoadPhi],
+	Global`$LoadPhi = False
+];
+If[ !ValueQ[Global`$LoadTARCER],
+	Global`$LoadTARCER = False
+];
 (* switch off startup messages by default: *)
-If[!ValueQ[Global`$FeynCalcStartupMessages], Global`$FeynCalcStartupMessages = False] ;
+If[ !ValueQ[Global`$FeynCalcStartupMessages],
+	Global`$FeynCalcStartupMessages = False
+];
 
-If[!ValueQ[Global`$FormLinkStartupMessages], Global`$FormLinkStartupMessages = False]; 
+If[ !ValueQ[Global`$FormLinkStartupMessages],
+	Global`$FormLinkStartupMessages = False
+];
 
 
-(* this setting will set the FeynCalc global variable $LeviCivitaSign = -I, such that 
+(* this setting will set the FeynCalc global variable $LeviCivitaSign = -I, such that
 FeynCalc uses the same convenction for traces involving Gamma[5]. *)
-If[!ValueQ[FeynCalcFormLink`$UseFormEpsConvention], FeynCalcFormLink`$UseFormEpsConvention = True];
+If[ !ValueQ[FeynCalcFormLink`$UseFormEpsConvention],
+	FeynCalcFormLink`$UseFormEpsConvention = True
+];
 
